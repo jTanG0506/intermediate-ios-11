41877991a3d8ea3bfefe23ec67db21d1066e420c
Jonathan Tang <jonathan.tang@student.manchester.ac.uk>
2018-03-21 18:53:07 +0000

    [C2] Customise appearance of section header view

--- IndexedTableDemo/IndexedTableDemo/AnimalTableViewController.swift
+++ IndexedTableDemo/IndexedTableDemo/AnimalTableViewController.swift
@@ -97,4 +97,21 @@ class AnimalTableViewController: UITableViewController {
     return index
   }
   
+  // MARK: - Table view delegate
+  override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+    return 50
+  }
+  
+  override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView,
+                          forSection section: Int) {
+    let headerView = view as! UITableViewHeaderFooterView
+    headerView.backgroundView?.backgroundColor = UIColor(red: 236.0 / 255.0,
+                                                         green: 240.0 / 255.0,
+                                                         blue: 241.0/255.0, alpha: 1.0)
+    headerView.textLabel?.textColor = UIColor(red: 231.0 / 255.0, green: 76.0 / 255.0,
+                                              blue: 60.0 / 255.0, alpha: 1.0)
+    
+    headerView.textLabel?.font = UIFont(name: "Avenir", size: 25.0)
+  }
+  
 }
