5b3f8980352676c06f42107ced03dfb19dcd64db
Jonathan Tang <jonathan.tang@student.manchester.ac.uk>
2018-03-21 13:54:03 +0000

    [C2] Create animals dictionary by first character of name
    Also create a method that creates the dictionary from a String array

--- IndexedTableDemo/IndexedTableDemo/AnimalTableViewController.swift
+++ IndexedTableDemo/IndexedTableDemo/AnimalTableViewController.swift
@@ -2,56 +2,71 @@
 //  AnimalTableViewController.swift
 //  IndexedTableDemo
 //
-//  Created by Simon Ng on 3/10/2016.
-//  Copyright © 2016 AppCoda. All rights reserved.
+//  Created by Jonathan Tang on 21/03/2018.
+//  Copyright © 2016 jtang0506. All rights reserved.
 //
 
 import UIKit
 
 class AnimalTableViewController: UITableViewController {
-    let animals = ["Bear", "Black Swan", "Buffalo", "Camel", "Cockatoo", "Dog", "Donkey", "Emu", "Giraffe", "Greater Rhea", "Hippopotamus", "Horse", "Koala", "Lion", "Llama", "Manatus", "Meerkat", "Panda", "Peacock", "Pig", "Platypus", "Polar Bear", "Rhinoceros", "Seagull", "Tasmania Devil", "Whale", "Whale Shark", "Wombat"]
+  
+  // MARK: - Properties
+  let animals = ["Bear", "Black Swan", "Buffalo", "Camel", "Cockatoo", "Dog", "Donkey", "Emu", "Giraffe", "Greater Rhea", "Hippopotamus", "Horse", "Koala", "Lion", "Llama", "Manatus", "Meerkat", "Panda", "Peacock", "Pig", "Platypus", "Polar Bear", "Rhinoceros", "Seagull", "Tasmania Devil", "Whale", "Whale Shark", "Wombat"]
+  
+  var animalsDict = [String: [String]]()
+  var animalsSectionTitles = [String]()
+  
+  // MARK: - View controller life cycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-        
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    // Generate the animal dictionary.
+    createAnimalDict()
+  }
+  
+  func createAnimalDict() {
+    for animal in animals {
+      // Get the first letter of the animals name to build the dictionary
+      let firstLetterIndex = animal.index(animal.startIndex, offsetBy: 1)
+      let animalKey = String(animal[..<firstLetterIndex])
+      
+      // Either add the animal to the array if it exists, or create a new array.
+      if var animalValues = animalsDict[animalKey] {
+        animalValues.append(animal)
+        animalsDict[animalKey] = animalValues
+      } else {
+        animalsDict[animalKey] = [animal]
+      }
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    // Get the section titles from dictionarys keys and sort them in ascending order.
+    animalsSectionTitles = [String](animalsDict.keys)
+    animalsSectionTitles.sort(by: { $0 < $1 })
+  }
+  
+  // MARK: - Table view data source
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    // Return the number of sections.
+    return 1
+  }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    // Return the number of rows in the section.
+    return animals.count
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
     
-    // MARK: - Table view data source
+    // Configure the cell...
+    cell.textLabel?.text = animals[indexPath.row]
     
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // Return the number of sections.
-        return 1
-    }
+    // Convert the animal name to lower case and
+    // then replace all occurences of a space with an underscore
+    let imageFileName = animals[indexPath.row].lowercased().replacingOccurrences(of: " ", with: "_")
+    cell.imageView?.image = UIImage(named: imageFileName)
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // Return the number of rows in the section.
-        return animals.count
-    }
-    
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        
-        // Configure the cell...
-        cell.textLabel?.text = animals[indexPath.row]
-        
-        // Convert the animal name to lower case and
-        // then replace all occurences of a space with an underscore
-        let imageFileName = animals[indexPath.row].lowercased().replacingOccurrences(of: " ", with: "_")
-        cell.imageView?.image = UIImage(named: imageFileName)
-        
-        return cell
-    }
-
-
+    return cell
+  }
+  
 }
