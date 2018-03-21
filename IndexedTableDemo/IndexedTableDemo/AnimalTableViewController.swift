d8ed9458b447caede7f4e97624c02523f0624a79
Jonathan Tang <jonathan.tang@student.manchester.ac.uk>
2018-03-21 13:39:50 +0000

    [C2] Add starter project

---
+++ IndexedTableDemo/IndexedTableDemo/AnimalTableViewController.swift
@@ -0,0 +1,57 @@
+//
+//  AnimalTableViewController.swift
+//  IndexedTableDemo
+//
+//  Created by Simon Ng on 3/10/2016.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class AnimalTableViewController: UITableViewController {
+    let animals = ["Bear", "Black Swan", "Buffalo", "Camel", "Cockatoo", "Dog", "Donkey", "Emu", "Giraffe", "Greater Rhea", "Hippopotamus", "Horse", "Koala", "Lion", "Llama", "Manatus", "Meerkat", "Panda", "Peacock", "Pig", "Platypus", "Polar Bear", "Rhinoceros", "Seagull", "Tasmania Devil", "Whale", "Whale Shark", "Wombat"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // Return the number of sections.
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of rows in the section.
+        return animals.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        // Configure the cell...
+        cell.textLabel?.text = animals[indexPath.row]
+        
+        // Convert the animal name to lower case and
+        // then replace all occurences of a space with an underscore
+        let imageFileName = animals[indexPath.row].lowercased().replacingOccurrences(of: " ", with: "_")
+        cell.imageView?.image = UIImage(named: imageFileName)
+        
+        return cell
+    }
+
+
+}
