36dea718dfa78451fccefe4d059c87558d22deb3
Jonathan Tang <jonathan.tang@student.manchester.ac.uk>
2018-04-06 12:10:06 +0100

    [C8] Request user location when in use
    Also change author and change indent to 2 spaces in starter files

--- MapKitDirection/MapKitDirection/MapViewController.swift
+++ MapKitDirection/MapKitDirection/MapViewController.swift
@@ -2,106 +2,119 @@
 //  MapViewController.swift
 //  MapKitDirection
 //
-//  Created by Simon Ng on 6/10/2016.
-//  Copyright © 2016 AppCoda. All rights reserved.
+//  Created by Jonathan Tang on 06/04/2018.
+//  Copyright © 2018 AppCoda. All rights reserved.
 //
 
 import UIKit
 import MapKit
 
 class MapViewController: UIViewController, MKMapViewDelegate {
-
-    @IBOutlet var mapView: MKMapView!
+  
+  @IBOutlet var mapView: MKMapView!
+  
+  var restaurant: Restaurant!
+  var locationManager = CLLocationManager()
+  
+  @IBAction func showDirection(sender: UIButton) {
     
-    var restaurant:Restaurant!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Convert address to coordinate and annotate it on map
-        let geoCoder = CLGeocoder()
-        geoCoder.geocodeAddressString(restaurant.location, completionHandler: { placemarks, error in
-            if let error = error {
-                print(error)
-                return
-            }
-            
-            if let placemarks = placemarks {
-                // Get the first placemark
-                let placemark = placemarks[0]
-                
-                // Add annotation
-                let annotation = MKPointAnnotation()
-                annotation.title = self.restaurant.name
-                annotation.subtitle = self.restaurant.type
-                
-                if let location = placemark.location {
-                    annotation.coordinate = location.coordinate
-                    
-                    // Display the annotation
-                    self.mapView.showAnnotations([annotation], animated: true)
-                    self.mapView.selectAnnotation(annotation, animated: true)
-                }
-            }
-            
-        })
-        
-        mapView.delegate = self
-        if #available(iOS 9.0, *) {
-            mapView.showsCompass = true
-            mapView.showsScale = true
-            mapView.showsTraffic = true
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Request for a user's authorisation for location services
+    locationManager.requestWhenInUseAuthorization()
+    let status = CLLocationManager.authorizationStatus()
+    
+    if status == CLAuthorizationStatus.authorizedWhenInUse {
+      mapView.showsUserLocation = true
     }
     
-    // MARK: - MKMapViewDelegate methods
+    // Convert address to coordinate and annotate it on map
+    let geoCoder = CLGeocoder()
+    geoCoder.geocodeAddressString(restaurant.location, completionHandler: { placemarks, error in
+      if let error = error {
+        print(error)
+        return
+      }
+      
+      if let placemarks = placemarks {
+        // Get the first placemark
+        let placemark = placemarks[0]
+        
+        // Add annotation
+        let annotation = MKPointAnnotation()
+        annotation.title = self.restaurant.name
+        annotation.subtitle = self.restaurant.type
+        
+        if let location = placemark.location {
+          annotation.coordinate = location.coordinate
+          
+          // Display the annotation
+          self.mapView.showAnnotations([annotation], animated: true)
+          self.mapView.selectAnnotation(annotation, animated: true)
+        }
+      }
+    })
     
-    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        let identifier = "MyPin"
-        
-        if annotation.isKind(of: MKUserLocation.self) {
-            return nil
-        }
-        
-        // Reuse the annotation if possible
-        var annotationView: MKAnnotationView?
-        
-        if #available(iOS 11.0, *) {
-            var markerAnnotationView: MKMarkerAnnotationView? = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView
-            
-            if markerAnnotationView == nil {
-                markerAnnotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
-                markerAnnotationView?.canShowCallout = true
-            }
-            
-            markerAnnotationView?.glyphText = "😋"
-            markerAnnotationView?.markerTintColor = UIColor.orange
-        
-            annotationView = markerAnnotationView
-            
-        } else {
-            
-            var pinAnnotationView: MKPinAnnotationView? = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView
-            
-            if pinAnnotationView == nil {
-                pinAnnotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
-                pinAnnotationView?.canShowCallout = true
-                pinAnnotationView?.pinTintColor = UIColor.orange
-            }
-            
-            annotationView = pinAnnotationView
-        }
-        
-        let leftIconView = UIImageView(frame: CGRect.init(x: 0, y: 0, width: 53, height: 53))
-        leftIconView.image = UIImage(named: restaurant.image)
-        annotationView?.leftCalloutAccessoryView = leftIconView
-            
-        return annotationView
+    mapView.delegate = self
+    
+    if #available(iOS 9.0, *) {
+      mapView.showsCompass = true
+      mapView.showsScale = true
+      mapView.showsTraffic = true
+    }
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  // MARK: - MKMapViewDelegate methods
+  
+  func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+    let identifier = "MyPin"
+    
+    if annotation.isKind(of: MKUserLocation.self) {
+      return nil
     }
     
+    // Reuse the annotation if possible
+    var annotationView: MKAnnotationView?
+    
+    if #available(iOS 11.0, *) {
+      var markerAnnotationView: MKMarkerAnnotationView? = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView
+      
+      if markerAnnotationView == nil {
+        markerAnnotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+        markerAnnotationView?.canShowCallout = true
+      }
+      
+      markerAnnotationView?.glyphText = "🍽"
+      markerAnnotationView?.markerTintColor = UIColor.orange
+      
+      annotationView = markerAnnotationView
+      
+    } else {
+      
+      var pinAnnotationView: MKPinAnnotationView? = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView
+      
+      if pinAnnotationView == nil {
+        pinAnnotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+        pinAnnotationView?.canShowCallout = true
+        pinAnnotationView?.pinTintColor = UIColor.orange
+      }
+      
+      annotationView = pinAnnotationView
+    }
+    
+    let leftIconView = UIImageView(frame: CGRect.init(x: 0, y: 0, width: 53, height: 53))
+    leftIconView.image = UIImage(named: restaurant.image)
+    annotationView?.leftCalloutAccessoryView = leftIconView
+    
+    return annotationView
+  }
+  
 }
--- MapKitDirection/MapKitDirection/Restaurant.swift
+++ MapKitDirection/MapKitDirection/Restaurant.swift
@@ -1,28 +1,28 @@
 //
-//  File.swift
+//  Restaurant.swift
 //  MapKitDirection
 //
-//  Created by Simon Ng on 6/10/2016.
-//  Copyright © 2016 AppCoda. All rights reserved.
+//  Created by Jonathan Tang on 06/04/2018.
+//  Copyright © 2018 AppCoda. All rights reserved.
 //
 
 import Foundation
 
 class Restaurant {
-    var name = ""
-    var type = ""
-    var location = ""
-    var image = ""
-    var isVisited = false
-    var phone = ""
-    var rating = ""
-    
-    init(name: String, type: String, location: String, phone: String, image: String, isVisited: Bool) {
-        self.name = name
-        self.type = type
-        self.location = location
-        self.phone = phone
-        self.image = image
-        self.isVisited = isVisited
-    }
+  var name = ""
+  var type = ""
+  var location = ""
+  var image = ""
+  var isVisited = false
+  var phone = ""
+  var rating = ""
+  
+  init(name: String, type: String, location: String, phone: String, image: String, isVisited: Bool) {
+    self.name = name
+    self.type = type
+    self.location = location
+    self.phone = phone
+    self.image = image
+    self.isVisited = isVisited
+  }
 }
