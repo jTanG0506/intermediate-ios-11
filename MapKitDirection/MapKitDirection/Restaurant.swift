166b8d2cf6e15bbdc19e9d3fb94583d79182819e
Jonathan Tang <jonathan.tang@student.manchester.ac.uk>
2018-04-06 11:21:43 +0100

    [C8] Add starter project

---
+++ MapKitDirection/MapKitDirection/Restaurant.swift
@@ -0,0 +1,28 @@
+//
+//  File.swift
+//  MapKitDirection
+//
+//  Created by Simon Ng on 6/10/2016.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import Foundation
+
+class Restaurant {
+    var name = ""
+    var type = ""
+    var location = ""
+    var image = ""
+    var isVisited = false
+    var phone = ""
+    var rating = ""
+    
+    init(name: String, type: String, location: String, phone: String, image: String, isVisited: Bool) {
+        self.name = name
+        self.type = type
+        self.location = location
+        self.phone = phone
+        self.image = image
+        self.isVisited = isVisited
+    }
+}
