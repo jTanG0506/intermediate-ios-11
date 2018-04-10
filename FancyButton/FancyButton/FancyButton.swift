3d3544bfcaed5b3070487875e84add78f144f82a
Jonathan Tang <jonathan.tang@student.manchester.ac.uk>
2018-04-10 16:24:45 +0100

    [C36] Add horizontal gradients to FancyButton

--- FancyButton/FancyButton/FancyButton.swift
+++ FancyButton/FancyButton/FancyButton.swift
@@ -85,12 +85,26 @@ class FancyButton: UIButton {
   // MARK: - Image Aligment
   @IBInspectable var enableImageRightAligned: Bool = false
   
+  
+  // MARK: - Gradient Background
+  @IBInspectable var enableGradientBackground: Bool = false
+  @IBInspectable var gradientColour1: UIColor = UIColor.black
+  @IBInspectable var gradientColour2: UIColor = UIColor.white
+  
   override func layoutSubviews() {
     super.layoutSubviews()
     
     if enableImageRightAligned, let imageView = imageView {
       imageEdgeInsets.left = self.bounds.width - imageView.bounds.width - imageRightPadding
     }
+    
+    if enableGradientBackground {
+      let gradientLayer = CAGradientLayer()
+      gradientLayer.frame = self.bounds
+      gradientLayer.colors = [gradientColour1.cgColor, gradientColour2.cgColor]
+      gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.5)
+      gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
+      self.layer.insertSublayer(gradientLayer, at: 0)
+    }
   }
-  
 }
