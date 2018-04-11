bf139f8ed4a03fe50ef4a2f816310fd31960ad9f
Jonathan Tang <jonathan.tang@student.manchester.ac.uk>
2018-04-11 14:02:31 +0100

    [C18] Create collection view controller and cell classes
    Also embed collection view controller in navigation controller

---
+++ CollectionViewDemo/CollectionViewDemo/IconCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  IconCollectionViewCell.swift
+//  CollectionViewDemo
+//
+//  Created by Jonathan Tang on 11/04/2018.
+//  Copyright © 2018 jtang0506. All rights reserved.
+//
+
+import UIKit
+
+class IconCollectionViewCell: UICollectionViewCell {
+  
+  @IBOutlet weak var iconImageView: UIImageView!
+  @IBOutlet weak var iconPriceLabel: UILabel!
+  
+}
