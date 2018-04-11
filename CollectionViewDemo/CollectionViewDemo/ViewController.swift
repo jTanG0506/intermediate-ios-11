06d0d524ee93a1e8a67d24c88424aba2c39e1c4f
Jonathan Tang <jonathan.tang@student.manchester.ac.uk>
2018-04-11 13:53:48 +0100

    [C18] Add starter project

---
+++ CollectionViewDemo/CollectionViewDemo/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  CollectionViewDemo
+//
+//  Created by Jonathan Tang on 11/04/2018.
+//  Copyright © 2018 jtang0506. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+}
