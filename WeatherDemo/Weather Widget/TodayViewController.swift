69f6a164f28c654e5a347b62a0321376c230e82d
Jonathan Tang <jonathan.tang@student.manchester.ac.uk>
2018-04-16 14:38:50 +0100

    [C21] Implement method for 'Weather Widget' to get data about Paris

--- WeatherDemo/Weather Widget/TodayViewController.swift
+++ WeatherDemo/Weather Widget/TodayViewController.swift
@@ -8,12 +8,32 @@
 
 import UIKit
 import NotificationCenter
+import WeatherInfoKit
 
 class TodayViewController: UIViewController, NCWidgetProviding {
   
+  // MARK: - Outlets
+  @IBOutlet var cityLabel: UILabel!
+  @IBOutlet var weatherLabel: UILabel!
+  @IBOutlet var temperatureLabel: UILabel!
+  
+  private var city = "Paris"
+  private var country = "France"
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view from its nib.
+    
+    cityLabel.text = "\(city), \(country)"
+    
+    // Invoke weather service to get the weather data
+    WeatherService.sharedWeatherService().getCurrentWeather(location: city) { (data) in
+      OperationQueue.main.addOperation {
+        if let weatherData = data {
+          self.weatherLabel.text = weatherData.weather.capitalized
+          self.temperatureLabel.text = String(format: "%d", weatherData.temperature) + "\u{00B0}"
+        }
+      }
+    }
   }
   
   override func didReceiveMemoryWarning() {
@@ -28,7 +48,21 @@ class TodayViewController: UIViewController, NCWidgetProviding {
     // If there's no update required, use NCUpdateResult.NoData
     // If there's an update, use NCUpdateResult.NewData
     
-    completionHandler(NCUpdateResult.newData)
+    cityLabel.text = city
+    
+    WeatherService.sharedWeatherService().getCurrentWeather(location: city) { (data) in
+      guard let weatherData = data else {
+        completionHandler(NCUpdateResult.noData)
+        return
+      }
+      
+      OperationQueue.main.addOperation {
+        self.weatherLabel.text = weatherData.weather.capitalized
+        self.temperatureLabel.text = String(format: "%d", weatherData.temperature) + "\u{00B0}"
+      }
+      
+      completionHandler(NCUpdateResult.newData)
+    }
   }
   
 }
