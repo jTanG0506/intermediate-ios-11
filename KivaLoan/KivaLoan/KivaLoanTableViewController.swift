79cd8e58d58e9a537f0094de461b606cb38a9c8f
Jonathan Tang <jonathan.tang@student.manchester.ac.uk>
2018-06-05 20:05:38 +0100

    [C4] Add Loan structure and methods to call Kiva API

--- KivaLoan/KivaLoan/KivaLoanTableViewController.swift
+++ KivaLoan/KivaLoan/KivaLoanTableViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class KivaLoanTableViewController: UITableViewController {
   
+  private let kivaLoanURL = "https://api.kivaws.org/v1/loans/newest.json"
+  private var loans = [Loan]()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
@@ -20,6 +23,59 @@ class KivaLoanTableViewController: UITableViewController {
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
+  }
+  
+  // MARK: - Data retrieval
+  
+  func getLatestLoans() {
+    guard let loanUrl = URL(string: kivaLoanURL) else {
+      return
+    }
+    
+    let request = URLRequest(url: loanUrl)
+    let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+      if let error = error {
+        print(error)
+        return
+      }
+      
+      // Parse JSON data
+      if let data = data {
+        self.loans = self.parseJsonData(data: data)
+        
+        // Reload table view
+        OperationQueue.main.addOperation {
+          self.tableView.reloadData()
+        }
+      }
+    }
+    
+    task.resume()
+  }
+  
+  func parseJsonData(data: Data) -> [Loan] {
+    var loans = [Loan]()
+    
+    do {
+      let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? NSDictionary
+      
+      // Parse JSON data
+      let jsonLoans = jsonResult?["loans"] as! [AnyObject]
+      for jsonLoan in jsonLoans {
+        var loan = Loan()
+        loan.name = jsonLoan["name"] as! String
+        loan.amount = jsonLoan["loan_amount"] as! Int
+        loan.use = jsonLoan["use"] as! String
+        
+        let location = jsonLoan["location"] as! [String: AnyObject]
+        loan.country = location["country"] as! String
+        loans.append(loan)
+      }
+    } catch {
+      print(error)
+    }
+    
+    return loans
   }
   
   // MARK: - Table view data source
---
+++ KivaLoan/KivaLoan/Loan.swift
@@ -0,0 +1,18 @@
+//
+//  Loan.swift
+//  KivaLoan
+//
+//  Created by Jonathan Tang on 05/06/2018.
+//  Copyright © 2018 AppCoda. All rights reserved.
+//
+
+import Foundation
+
+struct Loan {
+  
+  var name: String = ""
+  var country: String = ""
+  var use: String = ""
+  var amount: Int = 0
+  
+}
