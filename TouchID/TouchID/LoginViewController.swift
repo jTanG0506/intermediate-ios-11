038bc6f1658dfbffba0f52f0e6b760a93603fc48
Jonathan Tang <jonathan.tang@student.manchester.ac.uk>
2018-06-06 10:30:53 +0100

    [C28] Add biometric authentication via Touch ID and Face ID

--- TouchID/TouchID/LoginViewController.swift
+++ TouchID/TouchID/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class LoginViewController: UIViewController {
   
@@ -30,7 +31,8 @@ class LoginViewController: UIViewController {
     blurEffectView.frame = view.bounds
     backgroundImageView.addSubview(blurEffectView)
     
-    showLoginDialog()
+    loginView.isHidden = true
+    authenticateWithBiometric()
   }
   
   override func didReceiveMemoryWarning() {
@@ -51,6 +53,60 @@ class LoginViewController: UIViewController {
       
     }, completion: nil)
     
+  }
+  
+  func authenticateWithBiometric() {
+    // Get the local authentication context.
+    let localAuthContext = LAContext()
+    let reasonText = "Authentication is required to sign in"
+    var authError: NSError?
+    
+    if !localAuthContext.canEvaluatePolicy(
+      .deviceOwnerAuthenticationWithBiometrics, error: &authError) {
+      if let error = authError {
+        print(error.localizedDescription)
+      }
+      
+      // Display the login dialog when TouchID is not available.
+      showLoginDialog()
+      
+      return
+    }
+    
+    // Perform the biometric authentication
+    localAuthContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reasonText) { (success, error) in
+      if !success {
+        if let error = error {
+          switch error {
+          case LAError.authenticationFailed:
+            print("Authentication failed")
+          case LAError.passcodeNotSet:
+            print("Passcode not set")
+          case LAError.systemCancel:
+            print("Authentication was cancelled by the system")
+          case LAError.userCancel:
+            print("Authentication was cancelled by the user")
+          case LAError.biometryNotEnrolled:
+            print("Authentication could not start as user has not enrolled in either Touch ID or Face ID")
+          case LAError.biometryNotAvailable:
+            print("Authentication could not start because Touch ID or Face ID is not available")
+          case LAError.userFallback:
+            print("User tapped the fallback button")
+          default:
+            print(error.localizedDescription)
+          }
+          
+          // Fallback to password authentication
+          OperationQueue.main.addOperation {
+            self.showLoginDialog()
+          }
+        }
+      } else {
+        OperationQueue.main.addOperation {
+          self.performSegue(withIdentifier: "showHomeScreen", sender: nil)
+        }
+      }
+    }
   }
   
 }
